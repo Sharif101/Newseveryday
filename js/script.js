9a3e880fa0128cf753177c6120fc60b86af43758
Sharif101 <sharif009009@gmail.com>
2022-09-03 17:26:26 +0600

    upragde

--- js/script.js
+++ js/script.js
@@ -34,10 +34,10 @@ let displayNews =(data)=>{
         let newDiv = document.createElement('div');
         newDiv.classList.add('row');
         newDiv.innerHTML = `
-                <div class="col-md-4">
+                <div class="col-md-2 img">
                 <img src="${datas.thumbnail_url}" class="img-fluid rounded-start" alt="...">
                 </div>
-                <div class="col-md-8">
+                <div class="col-md-10">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
